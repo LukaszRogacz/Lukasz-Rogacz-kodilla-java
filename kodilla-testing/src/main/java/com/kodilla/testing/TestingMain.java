979f4b17e5f417c897ec8e497410dc027c7b4779
“LukaszRogacz <lukasz_rogacz@wp.pl>
2018-04-13 15:32:52 +0200

    6.2. First unit tests (class Calculator)

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,37 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
+
 public class TestingMain {
-    public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+    public static void main(String[] args) {
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        System.out.println("Test - first unit test; method add from class Calculator:");
+
+        Calculator myCalculator = new Calculator();
+
+        if (myCalculator.add(2, 3) == 5) {
+            System.out.println("test(class Calculator; method add) OK");
+        } else {
+            System.out.println("Error! (class Calculator; method add)");
+        }
+
+        System.out.println("Test - second unit test; method subtract from class Calculator:");
+
+        if (myCalculator.subtract(21, 3) == 18) {
+            System.out.println("test(class Calculator; method subtract) OK");
+        } else {
+            System.out.println("Error! (class Calculator; method subtract)");
+        }
     }
 }
