666653c4eae9f6e7f245ade80bc8dca163c28554
“LukaszRogacz <lukasz_rogacz@wp.pl>
2018-04-29 20:37:36 +0200

    8.2 try-catch-finally-poprawka

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -3,39 +3,33 @@ package com.kodilla.exception.test;
 public class FirstChallenge {
 
     public double divide(double a, double b) throws ArithmeticException {
-
-        try{
-            if(b == 0){
-                throw new ArithmeticException();
-            }
-
+        if (b == 0) {
+            throw new ArithmeticException();
         }
-        catch (Exception e){
-            System.out.println("Division by 0. Error"+e);
-        }
-        finally {
-
-            if(b==0){
-                System.out.println("As a result of division there is returned the highest value");
-                return Double.MAX_VALUE;
-            } else {
-                return a / b;
-            }
-        }
-        }
+        return a / b;
+    }
 
     /**
      * This main can throw an ArithmeticException!!!
+     *
      * @param args
      */
+
     public static void main(String[] args) {
 
         FirstChallenge firstChallenge = new FirstChallenge();
+        double result;
+        try {
+            result = firstChallenge.divide(3, 0);
+        } catch (Exception e) {
+            System.out.println("Division by 0. Error" + e);
+        } finally {
 
-        double result = firstChallenge.divide(3, 0);
+            System.out.println("As a result of division by 0, there is returned the highest value");
+            result = Double.MAX_VALUE;
+            System.out.println(result);
+        }
 
-
-        System.out.println(result);
 
     }
 }
