750c49682e09b414a6c5e4f89cfca420fe1e2d62
“LukaszRogacz <lukasz_rogacz@wp.pl>
2018-06-03 22:10:18 +0200

    13.2 Hibernate.Tworzymy encję list zadań-poprawka

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/SpringRunner.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/SpringRunner.java
@@ -1,4 +1,4 @@
-package com.kodilla.hibernate.tasklist;
+package com.kodilla.hibernate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
