d3b6dd6a8764a1b05ff69f684b1f4ca16272b8c3
“LukaszRogacz <lukasz_rogacz@wp.pl>
2018-09-20 21:37:35 +0200

    13. Hibernate-czyszczenie-update

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/Task.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/Task.java
@@ -1,8 +1,36 @@
 package com.kodilla.hibernate.task;
 
+import com.kodilla.hibernate.tasklist.TaskList;
+import org.springframework.data.jpa.repository.Query;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.util.Date;
+
+
+
+@NamedQueries({
+        @NamedQuery(
+                name="Task.retrieveLongTasks",
+                query="FROM Task WHERE duration>10"
+        ),
+        @NamedQuery(
+                name="Task.retrieveShortTasks",
+                query="FROM Task WHERE duration<=10"
+        ),
+        @NamedQuery(
+                name="Task.retrieveTasksWithDurationLongerThan",
+                query="FROM Task WHERE duration> :DURATION"
+        )
+})
+
+@NamedNativeQuery(
+        name="Task.retrieveTasksWithEnoughTime",
+        query="SELECT * FROM TASKS"+
+        " WHERE DATEDIFF(DATE_ADD(CREATED,INTERVAL DURATION DAY),NOW())>5",
+        resultClass = Task.class
+)
+
 
 @Entity
 @Table(name = "TASKS")
@@ -11,6 +39,8 @@ public final class Task {
     private String description;
     private Date created;
     private int duration;
+    private TaskFinancialDetails taskFinancialDetails;
+    private TaskList taskList;
 
     public Task() {
     }
@@ -19,6 +49,25 @@ public final class Task {
         this.description = description;
         this.created = new Date();
         this.duration = duration;
+    }
+    @ManyToOne
+    @JoinColumn(name="TASK_LIST_ID")
+    public TaskList getTaskList() {
+        return taskList;
+    }
+
+    public void setTaskList(TaskList taskList) {
+        this.taskList = taskList;
+    }
+
+    @OneToOne(cascade = CascadeType.ALL,fetch = FetchType.EAGER)
+    @JoinColumn(name="TASK_FINANCIAL_ID")
+    public TaskFinancialDetails getTaskFinancialDetails() {
+        return taskFinancialDetails;
+    }
+
+    public void setTaskFinancialDetails(TaskFinancialDetails taskFinancialDetails) {
+        this.taskFinancialDetails = taskFinancialDetails;
     }
 
     @Id
